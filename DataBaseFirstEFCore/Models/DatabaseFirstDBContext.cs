c26f66492f77f86ba3f608b1c6968db0bf615a06
Arpit Mhaisne <arpitmh25@gmail.com>
2025-01-14 01:51:40 +0530

    Add project files.

---
+++ DataBaseFirstEFCore/Models/DatabaseFirstDBContext.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DataBaseFirstEFCore.Models
+{
+    public partial class DatabaseFirstDBContext : DbContext
+    {
+        public DatabaseFirstDBContext()
+        {
+        }
+
+        public DatabaseFirstDBContext(DbContextOptions<DatabaseFirstDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; } = null!;
+        public virtual DbSet<Student> Students { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.ToTable("Employee");
+
+                entity.Property(e => e.Designation)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Salary)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.Property(e => e.StudentGender)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.StudentName)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ DataBaseFirstEFCore/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataBaseFirstEFCore.Models
+{
+    public partial class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Designation { get; set; } = null!;
+        public string Salary { get; set; } = null!;
+    }
+}
